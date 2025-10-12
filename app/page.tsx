957a2ed3f259d657c44cdbf5691edc45c125b587
Li0nbl4ze <thedbgamer01@gmail.com>
2025-10-12 22:04:15 +0200

    Update page.tsx
    Emailänderung für Support

--- app/page.tsx
+++ app/page.tsx
@@ -329,7 +329,7 @@ export default function Home() {
             <h4 className="text-white/90 font-medium mb-4">Kontakt</h4>
             <div className="space-y-2">
                 <div className="text-gray-400 text-sm mb-4">
-                Support: <Link href="mailto:timelesscityinfo@gmail.com" className="hover:text-white">timelesscityinfo@gmail.com</Link>
+                Support: <Link href="mailto:verwaltung.timeless@gmail.com" className="hover:text-white">verwaltung.timeless@gmail.com</Link>
                 </div>
               <SocialLinks />
             </div>
